f3828e8d4d857a4d0ab38a920ef294a614a68936
Jens Henrik Vogelius <jenvog@norlys.dk>
2021-12-16 15:14:01 +0100

    Load recipes from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+interface Recipe {
+  name: string;
+}
 
 function App() {
+
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = () => {
+    fetch('https://brewlog-api.azurewebsites.net/recipes').then(response => {
+      setIsLoading(true);
+      console.log("response", response);
+      return response.json();
+    }).then(data => {
+      console.log("data", data);
+      setRecipes(data);
+      setIsLoading(false);
+    }).catch(error => {
+      console.log("error", error);
+      setIsLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="#"
-        >
-          Brewlog
-        </a>
+        
       </header>
+      <body>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading &&
+        <ul>
+          { recipes.map(r => <li>{r.name}</li>)}
+        </ul>}
+      </body>
     </div>
   );
 }
